77b6fe3113421929d462ab3c7d5d32ecc23e5740
tamkde <niekto@gmail.com>
2025-01-13 16:23:32 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,1 +1,9 @@
-
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $file = fopen("data.txt", "a");
+    fwrite($file, $name . "\n");
+    fclose($file);
+    echo "Data written to file.";
+}
+?>
