5958c9f7dc0dd535b6f8df6e9c9949859adde07b
tamkde <niekto@gmail.com>
2025-01-13 16:44:40 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
-    $file = fopen("data.txt", "a");
+    $file = fopen("data.html", "a");
     fwrite($file, $name . "\n");
     fclose($file);
     echo "Data written to file.";
